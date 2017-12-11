a919f0d3ce6a3323a98c562ba3b02484e13764a3
Andrew Murray <darbyguitars@gmail.com>
2017-12-11 17:39:22 -0600

    added some of the basic react router and also created our higher order component that will be unaware of login credintials

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipie
+      <ul>
+        <li>1 tray of Betty Crocker Bownie Mix</li>
+        <li>1 cup of canned tuna</li>
+        <li>1 jar of Mayo</li>
+      </ul>
+    </div>
+  )
+}
