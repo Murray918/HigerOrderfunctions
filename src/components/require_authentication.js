142e6a373b36a45a9422936140e3b4f5848e5e62
Andrew Murray <darbyguitars@gmail.com>
2017-12-12 13:31:44 -0600

    there are no errors and the rout now pushes the user back to the home page if the user is not logged in

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,9 +3,23 @@ import { connect } from "react-redux";
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    //context is like props but skips the component higherarchy but to keep this from being abused it requires you to define such context like we do here
     static contextTypes = {
-      router : React.ProtoTypes.obejct
+      router : React.PropTypes.object.isRequired
     }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/')
+      }
+    }
+
+    componentWillUpdate(nextProps){
+      if (!nextProps.authenticated){
+        this.context.router.push('/')
+      }
+    }
+
     render() {
       console.log(this.context);
       return <ComposedComponent {...this.props} />;
